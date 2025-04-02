51ecedc384ad3986ebfc2d6cae66e00b9cfcc3d7
emile <emilm7843@gmail.com>
2025-04-02 10:28:40 +0200

    Update report

--- run_and_update.sh
+++ run_and_update.sh
@@ -1,4 +1,5 @@
-python main.py > README.md
+C:/Users/emile/AppData/Local/Programs/Python/Python312/python.exe main.py > README.md
 git add replays/*
 git add README.md
-git commit -m "Update report"
+git commit -a -m "Update report"
+git push origin master
